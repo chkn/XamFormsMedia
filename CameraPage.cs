70cdda933c46d56f12381cc3f01b6d9cb494279c
Alex Corrado <alexc@xamarin.com>
2015-12-02 18:50:11 -0800

    Initial commit

---
+++ CameraPage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace XamFormsMedia {
+
+	public enum CameraPreference {
+		Rear,
+		Front
+	}
+
+	public class CameraPage : Page {
+
+		public static readonly BindableProperty PreferredCameraProperty = BindableProperty.Create (nameof (PreferredCamera), typeof (CameraPreference), typeof (CameraPage), default (CameraPreference));
+
+		internal static readonly BindablePropertyKey ImagePropertyKey  = BindableProperty.CreateReadOnly (nameof (Image), typeof (FileImageSource), typeof (CameraPage), null);
+		public static readonly BindableProperty ImageProperty = ImagePropertyKey.BindableProperty;
+
+		public CameraPreference PreferredCamera {
+			get { return (CameraPreference) GetValue (PreferredCameraProperty); }
+			set { SetValue (PreferredCameraProperty, value); }
+		}
+
+		public FileImageSource Image {
+			get { return (FileImageSource) GetValue (ImageProperty); }
+			internal set { SetValue (ImagePropertyKey, value); }
+		}
+
+		public event EventHandler Completed;
+
+		protected internal virtual void OnCompleted ()
+		{
+			var completed = Completed;
+			if (completed != null)
+				completed (this, EventArgs.Empty);
+		}
+
+	}
+}
+
---
+++ XamFormsMedia.iOS/CameraPageRenderer.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+using UIKit;
+using Foundation;
+using ObjCRuntime;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+
+namespace XamFormsMedia.iOS {
+
+	public class CameraPageRenderer : PageRenderer
+	{
+		UIImagePickerController picker;
+
+		public new CameraPage Element {
+			get { return (CameraPage)base.Element; }
+		}
+
+		public CameraPageRenderer ()
+		{
+			picker = new UIImagePickerController {
+				SourceType = UIImagePickerControllerSourceType.Camera,
+				CameraCaptureMode = UIImagePickerControllerCameraCaptureMode.Photo
+			};
+			picker.Canceled += delegate {
+				Element.OnCompleted ();
+			};
+			picker.FinishedPickingImage += (_, e) => {
+				var dir = Runtime.GetNSObject<NSString> (NSTemporaryDirectory ()).ToString ();
+				var path = Path.Combine (dir, "fartbum.png");
+				e.Image.AsPNG ().Save (path);
+				Element.Image = path;
+				Element.OnCompleted ();
+			};
+		}
+
+		protected override void OnElementChanged (VisualElementChangedEventArgs e)
+		{
+			base.OnElementChanged (e);
+			if (e.OldElement != null) {
+				e.OldElement.PropertyChanged -= HandlePropertyChanged;
+			}
+			if (e.NewElement != null) {
+				e.NewElement.PropertyChanged += HandlePropertyChanged;
+				UpdateBindings ();
+			}
+		}
+
+		void HandlePropertyChanged (object sender, PropertyChangedEventArgs e)
+		{
+			UpdateBindings ();
+		}
+
+		void UpdateBindings ()
+		{
+			switch (Element.PreferredCamera) {
+
+			case CameraPreference.Front:
+				picker.CameraDevice = UIImagePickerControllerCameraDevice.Front;
+				break;
+
+			case CameraPreference.Rear:
+				picker.CameraDevice = UIImagePickerControllerCameraDevice.Rear;
+				break;
+			}
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			AddChildViewController (picker);
+			picker.View.Frame = View.Bounds;
+			View.AddSubview (picker.View);
+			picker.DidMoveToParentViewController (this);
+		}
+
+		[DllImport (Constants.FoundationLibrary)]
+		static extern IntPtr NSTemporaryDirectory ();
+	}
+}
+
